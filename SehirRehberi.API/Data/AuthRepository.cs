0a972c20f519e0386c9d3e9fc581b5cfe9c01caf
ozgu kaptan <ozgukp@hotmail.com>
2020-09-30 19:08:14 +0300

    AuthRegister

---
+++ SehirRehberi.API/Data/AuthRepository.cs
@@ -0,0 +1,50 @@
+﻿using SehirRehberi.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SehirRehberi.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public Task<User> Login(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public Task<bool> UserExists(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
